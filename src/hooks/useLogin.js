9d3df318dffd27c28395aa8a8161059dc6f63a97
Abdelrahman Kasem <abdelrahmankasem7@gmail.com>
2023-08-01 09:28:48 +0300

    one

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import {useAuthContext} from './useAuthContext'
 
 export const useLogin = () => {
-    const [error, setError] = useState(null)
+    const [error, setError] = useState("")
     const [isLoading, setIsLoading] = useState(null)
     const {dispatch} = useAuthContext()
     
