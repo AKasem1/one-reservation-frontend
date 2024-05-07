41d711b1efd38d3bea74b1141d86090a8dd8f2b3
Abdelrahman Kasem <87794262+AKasem1@users.noreply.github.com>
2024-05-07 12:56:04 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const backendURL = "https://one-reservation-system.onrender.com";
+const backendURL = "https://one-reservation-backend.onrender.com";
 // const backendURL = "http://localhost:2024";
 //http://localhost:2024
 //https://one-reservation-system.onrender.com
