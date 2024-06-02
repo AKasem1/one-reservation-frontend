3a242712bb8aadd8e80be101ef90378fc0cf22c2
AKasem1 <abdelrahmankasem7@gmail.com>
2024-06-02 19:19:05 +0300

    2025

--- src/config.js
+++ src/config.js
@@ -1,8 +1,5 @@
 const backendURL = "https://one-reservation-backend.onrender.com";
-<<<<<<< HEAD
 //const backendURL = "https://one-reservation-system.onrender.com";
-=======
->>>>>>> 4ddc0171f99925678784de595a03d232a1dce302
 // const backendURL = "http://localhost:2024";
 //http://localhost:2024
 //https://one-reservation-system.onrender.com
